a0b70251d362597c7a16d56c8b76239edbf82693
Tawit68 <684259011@webmail.npru.ac.th>
2026-03-06 12:07:04 +0700

    Add files via upload

--- select2.php
+++ select2.php
@@ -7,7 +7,10 @@
 <body>
     <?php
     require "connect.php";
-    $sql = "SELECT * FROM customer";
+    $sql = "SELECT customer.CustomerID, customer.Name, customer.Email, country.CountryName
+FROM Customer, country
+WHERE customer.CountryCode = country.CountryCode AND country.CountryName LIKE 'ไท%'
+";
 
     $stmt = $conn->prepare($sql);
     $stmt->execute();
@@ -21,37 +24,37 @@
             <th width="140">
                 <div align="center">ชื่อ</div>
             </th>
-            <th width="120">
+            <!-- <th width="120">
                 <div align="center">วันเกิด </div>
-            </th>
+            </th> -->
             <th width="100">
                 <div align="center">อีเมล์ </div>
             </th>
             <th width="50">
                 <div align="center">ประเทศ </div>
             </th>
-            <th width="70">
+            <!-- <th width="70">
                 <div align="center">ยอดหนี้ </div>
-            </th>
+            </th> -->
+        </tr> 
+            <?php
+            while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            ?>
+        <tr>
+            <td>
+                <a href="detail.php?CustomerID=<?php echo $result['CustomerID']; ?>">
+                    <?php echo $result["CustomerID"]; ?>
+                </a>
+            </td>
+            <td><?php echo $result["Name"]; ?></td>
+            <!-- <td><?php echo $result["Birthdate"]; ?></td> -->
+            <td><?php echo $result["Email"]; ?></td>
+            <td><?php echo $result["CountryName"]; ?></td>
+            <!-- <td><?php echo $result["OutstandingDebt"]; ?></td> -->
         </tr>
-        <?php
-        while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        ?>
-            <tr>
-                <td>
-                    <a href="detail.php?CustomerID=<?php echo $result['CustomerID']; ?>">
-                        <?php echo $result["CustomerID"]; ?>
-                    </a>
-                </td>
-                <td><?php echo $result["Name"]; ?></td>
-                <td><?php echo $result["Birthdate"]; ?></td>
-                <td><?php echo $result["Email"]; ?></td>
-                <td><?php echo $result["CountryCode"]; ?></td>
-                <td><?php echo $result["OutstandingDebt"]; ?></td>
-            </tr>
-        <?php
-        }
-        ?>
+    <?php
+            }
+    ?>
     </table>
     <?php
     $conn = null;
